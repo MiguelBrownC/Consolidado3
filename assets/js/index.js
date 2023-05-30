46073d27ee66b64e8b585e7b48535eb192227169
M.Brown <browncoloma@gmailcom>
2023-05-30 11:52:50 -0400

    algo de avance e incorporacion d css

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,7 +21,9 @@ let inputPresupuesto = document.getElementById("presupuesto")
 inputPresupuesto.addEventListener('keyup', function () {
 	let presupuesto = inputPresupuesto.value.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 	inputPresupuesto.value = presupuesto
-	console.log(inputPresupuesto.value)
+	prueba=inputPresupuesto.value.replace(/\D/g, "")
+	// console.log(inputPresupuesto.value)
+	// console.log(prueba)
 	document.getElementById("totalPresupuesto").innerHTML = `CLP: ${presupuesto}`
 
 })
@@ -29,23 +31,49 @@ inputPresupuesto.addEventListener('keyup', function () {
 function agregar(){
 	
 	valormas = inputPresupuesto.value
+	prueba=valormas.replace(/\D/g, "")
 
-	document.getElementById("clpabono").innerHTML = valormas
+	document.getElementById("clpabono").innerHTML = prueba
+
+	action()
 }
 
 function gasto(){
-	valormas = inputPresupuesto.value
-
-	document.getElementById("clpgastos").innerHTML = valormas
+	valormenos = inputPresupuesto.value
+	prueba2=valormenos.replace(/\D/g, "")
+	document.getElementById("clpgastos").innerHTML = prueba2
+	action()
 }
 
-
-let balance = document.getElementById("presupuesto")
-inputPresupuesto.addEventListener('keyup', function () {
-	let a = parseFloat( document.getElementById("clpabono"))
-	let g = parseFloat( 	document.getElementById("clpgastos"))
+function action(){
+	let aa=0
+	let a = document.getElementById("clpabono")
+	aa = parseFloat(a)
+	console.log(a)
+	let gg = 0
+	let g = document.getElementById("clpgastos")
+	gg=parseFloat(g)
 	let total = 0
-	total = a - g
+	total = aa - gg
 	console.log(total)
-	document.getElementById("balance").innerHTML = `CLP: ${total}`
-})
+	document.getElementById("balance").innerHTML = total
+}
+
+// let balance = document.getElementById("presupuesto")
+// inputPresupuesto.addEventListener('keyup', function () {
+// 	let a = parseFloat( document.getElementById("clpabono"))
+// 	let g = parseFloat( 	document.getElementById("clpgastos"))
+// 	let total = 0
+// 	total = a - g
+// let balance = document.getElementById("presupuesto")
+// inputPresupuesto.addEventListener('keyup', function () {
+// 	let a = parseFloat( document.getElementById("clpabono"))
+// 	let g = parseFloat( 	document.getElementById("clpgastos"))
+// 	let total = 0
+// 	total = a - g
+// 	console.log(total)
+// 	document.getElementById("balance").innerHTML = `CLP: ${total}`
+// })
+// 	console.log(total)
+// 	document.getElementById("balance").innerHTML = `CLP: ${total}`
+// })
