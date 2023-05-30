32c6a4b78728360db259b9b6f13ebc5f04e96e97
M.Brown <browncoloma@gmailcom>
2023-05-29 23:48:52 -0400

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,51 @@
+const productos = [
+	{
+		id: 1,
+		nombre: "abono",
+		color: "green",
+		precio: 0.
+	},
+	{
+		id: 2,
+		nombre: "Gastos",
+		color: "red",
+		precio: 0.
+	},
+
+
+]
+
+
+
+let inputPresupuesto = document.getElementById("presupuesto")
+inputPresupuesto.addEventListener('keyup', function () {
+	let presupuesto = inputPresupuesto.value.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+	inputPresupuesto.value = presupuesto
+	console.log(inputPresupuesto.value)
+	document.getElementById("totalPresupuesto").innerHTML = `CLP: ${presupuesto}`
+
+})
+
+function agregar(){
+	
+	valormas = inputPresupuesto.value
+
+	document.getElementById("clpabono").innerHTML = valormas
+}
+
+function gasto(){
+	valormas = inputPresupuesto.value
+
+	document.getElementById("clpgastos").innerHTML = valormas
+}
+
+
+let balance = document.getElementById("presupuesto")
+inputPresupuesto.addEventListener('keyup', function () {
+	let a = parseFloat( document.getElementById("clpabono"))
+	let g = parseFloat( 	document.getElementById("clpgastos"))
+	let total
+	total = a - b
+	console.log(inputPresupuesto.value)
+	document.getElementById("balance").innerHTML = `CLP: ${presupuesto}`
+})
