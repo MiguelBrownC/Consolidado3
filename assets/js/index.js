0534a495c2034c7b4ec6b5a2e5aed70ba17f2db3
M.Brown <browncoloma@gmailcom>
2023-05-31 10:35:38 -0400

    listado de gastos y pintado de blance

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,60 +1,67 @@
-const productos = [
-	{
-		id: 1,
-		nombre: "abono",
-		color: "green",
-		precio: 0.
-	},
-	{
-		id: 2,
-		nombre: "Gastos",
-		color: "red",
-		precio: 0.
-	},
+const gastos = []
 
-
-]
-
-
-
+let total = 0
 let inputPresupuesto = document.getElementById("presupuesto")
 inputPresupuesto.addEventListener('keyup', function () {
 	let presupuesto = inputPresupuesto.value.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 	inputPresupuesto.value = presupuesto
-	prueba=inputPresupuesto.value.replace(/\D/g, "")
-	// console.log(inputPresupuesto.value)
-	// console.log(prueba)
+	prueba = inputPresupuesto.value.replace(/\D/g, "")
 	document.getElementById("totalPresupuesto").innerHTML = `CLP: ${presupuesto}`
-
 })
 
-function agregar(){
-	
+function agregar() {
 	valormas = inputPresupuesto.value
-	prueba=valormas.replace(/\D/g, "")
-
+	prueba = valormas.replace(/\D/g, "")
 	document.getElementById("clpabono").innerHTML = prueba
-
 	action()
 }
 
-function gasto(){
-	valormenos = inputPresupuesto.value
-	prueba2=valormenos.replace(/\D/g, "")
-	document.getElementById("clpgastos").innerHTML = prueba2
-	action()
+function eliminarGasto(id){
+console.log(id)
 }
 
-function action(){
-	let aa=0
+function imprimirGastos(){
+	let divGastos = document.getElementById("gastosTable")
+	let html= ''
+	gastos.forEach(gasto =>{
+		html += `
+		<tr>
+		<td>${gasto.id}</td>
+		<td>${gasto.nombre}</td>
+		<td>${gasto.monto}</td>
+		<td><button class="btn btn-danger" onclick="eliminarGastos(${gasto.id})">Borrar</button></td>
+	  </tr>
+		`	
+	})
+	divGastos.innerHTML = html
+	action()
+}	
+
+function gasto() {
+	let nombre = document.getElementById("nombreGasto").value
+	let monto = document.getElementById("montoGasto").value
+	gastos.push({ id: gastos.length + 1, nombre: nombre, monto: monto })
+	imprimirGastos()
+}
+
+// function gasto(){
+// 	valormenos = inputPresupuesto.value
+// 	prueba2=valormenos.replace(/\D/g, "")
+// 	document.getElementById("clpgastos").innerHTML = prueba2
+// 	action()
+// }
+
+function action() {
+	let totalGastos = gastos.reduce((acumulador, valorActual) => acumulador +  parseFloat(valorActual.monto),0)
+	document.getElementById("clpgastos").innerHTML = totalGastos
+	console.log(totalGastos)
+	let aa = 0.0
 	let a = document.getElementById("clpabono")
-	aa = parseFloat(a)
-	console.log(a)
+	aa = parseFloat(a.textContent)
 	let gg = 0
 	let g = document.getElementById("clpgastos")
-	gg=parseFloat(g)
-	let total = 0
-	total = aa - gg
+	gg = parseFloat(g.textContent)
+	total = aa - totalGastos
 	console.log(total)
 	document.getElementById("balance").innerHTML = total
 }
